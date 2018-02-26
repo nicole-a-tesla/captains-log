9f6bf80d5085a7897e81c2315ee1b0059a7b2f58
nicole-a-tesla <nmccabe@8thlight.com>
2018-02-25 21:31:01 -0500

    Add MaterialUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import SpeciesContainer from './species-container'
 
-import SpeciesList from './species-list'
-
-var species = [
-  {
-    name: 'Terrellian',
-    origin: 'Unknown, Alpha Quadrant',
-    attributes: ['humanoid'],
-    warpCapable: 'false',
-    sightings: 2
-  },
-  {
-    name: 'Vulcan',
-    origin: 'Vulcan, Alpha Quadrant',
-    warpCapable: 'true',
-    attributes: ['humanoid','telepathic'],
-    sightings: 400
-  }
-]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Captain's Log 💫  🚀  👽 </h1>
-        </header>
-        <SpeciesList
-          species={ species }
-          onSightingsUpdate={ (e)=>{ console.log(e) } }
-          onRemove={ ()=>{ console.log('Removed!') } }
-        />
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Captain's Log 💫  🚀  👽 </h1>
+          </header>
+          <SpeciesContainer />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
